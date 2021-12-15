6a9dce99673dfee77ce70a3dee9856230383489b
EliMeitner <eli.meitner@toluna.com>
2021-12-15 09:23:55 +0200

    Update main.tf

--- examples/cloudwatch-to-coralogix/main.tf
+++ examples/cloudwatch-to-coralogix/main.tf
@@ -2,6 +2,6 @@ module "coralogix" {
   source                      = "../../"
   version                     = "~>0.0.1"
   app_name                    = "my_app"
-  loggroup_envs               = ['qa','dev','prod']
+  loggroup_envs               = ["qa","dev","prod"]
   region                      = "us-east-1"
 }
